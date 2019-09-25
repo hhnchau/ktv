3235b27eeb55c4bdd78b7943e611730c93e3f569
NhuHoang <nhuhtk@hanet.com>
2019-09-25 16:38:20 +0700

    update search soundclound, youtube

--- TouchSreenTab/app/src/main/java/com/vk2/touchsreentab/adapter/YoutubeAdapter.java
+++ TouchSreenTab/app/src/main/java/com/vk2/touchsreentab/adapter/YoutubeAdapter.java
@@ -8,11 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.vk2.touchsreentab.R;
-import com.vk2.touchsreentab.databinding.ItemSoundcloudBinding;
+import com.vk2.touchsreentab.database.entity.Song;
 import com.vk2.touchsreentab.databinding.ItemYoutubeBinding;
-import com.vk2.touchsreentab.model.Song;
-import com.vk2.touchsreentab.model.SoundCloud;
-import com.vk2.touchsreentab.model.Youtube;
 
 import java.util.List;
 
